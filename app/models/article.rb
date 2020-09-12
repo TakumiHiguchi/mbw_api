452649c2093f227f6839d4743cee379bb077309e
TakumiHiguchi <andf.for.gt@gmail.com>
2020-09-13 03:36:55 +0900

    [fix] s3の設定

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,12 +4,14 @@ class Article < ApplicationRecord
     has_many :tags, through: :article_tag_relations
 
     def image_from_base64(b64)
-        bin = Base64.decode64(b64)
-        file = Tempfile.new('img')
-        file.binmode
-        file << bin
-        file.rewind
-      
-        self.update(thumbnail:file)
+        if !b64.nil?
+            bin = Base64.decode64(b64)
+            file = Tempfile.new('img')
+            file.binmode
+            file << bin
+            file.rewind
+        
+            self.update(thumbnail:file)
+        end
     end
 end
