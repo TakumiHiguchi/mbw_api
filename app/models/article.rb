6b38feb419a89bc734fac0821f56578afd75cea5
TakumiHiguchi <andf.for.gt@gmail.com>
2020-09-13 07:34:01 +0900

    [add] s3設定

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,12 +4,17 @@ class Article < ApplicationRecord
     has_many :tags, through: :article_tag_relations
 
     def image_from_base64(b64)
-        bin = Base64.decode64(b64)
-        file = Tempfile.new('img')
-        file.binmode
-        file << bin
-        file.rewind
-      
-        self.update(thumbnail:file)
+        uri = URI.parse(b64)
+        if uri.scheme == "data" then
+            opaque = uri.opaque
+            data = opaque[opaque.index(",") + 1, opaque.size]
+            bin = Base64.decode64(data)
+            file = Tempfile.new('img')
+            file.binmode
+            file << bin
+            file.rewind
+          
+            self.update(thumbnail:file)
+        end
     end
 end
