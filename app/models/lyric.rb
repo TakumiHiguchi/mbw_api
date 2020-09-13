cb384b51781bcd3c62631658e2f1350257f11435
TakumiHiguchi <andf.for.gt@gmail.com>
2020-09-13 11:44:28 +0900

    [add] 簡単な検索APIの作成

--- app/models/lyric.rb
+++ app/models/lyric.rb
@@ -3,11 +3,24 @@ class Lyric < ApplicationRecord
     has_many :favs
     def self.search(props)
         return self.all.limit(props[:limit]) unless props[:query]
-        result = self.where(
+        ins = self.where(
             ['artist ? OR title LIKE ?', 
             "%#{props[:query].upcase}%", "%#{props[:query].upcase}%"]
         ).limit(props[:limit])
 
+        result = ins.map do |data|
+            next({
+                title:data.title,
+                artist:data.artist,
+                key:data.key,
+                jucket:data.jucket.to_s,
+                lyricist:data.lyricist,
+                composer:data.composer,
+                lyrics:data.lyrics,
+                amazonUrl:data.amazonUrl,
+                iTunesUrl:data.iTunesUrl,
+            })
+        end
         return result
     end
     
