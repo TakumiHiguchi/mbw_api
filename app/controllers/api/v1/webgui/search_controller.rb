66427dd96d9dbe38432a5bd237716f7285ec02cc
TakumiHiguchi <andf.for.gt@gmail.com>
2020-09-13 11:47:01 +0900

    [add] 簡単な検索APIの作成

--- app/controllers/api/v1/webgui/search_controller.rb
+++ app/controllers/api/v1/webgui/search_controller.rb
@@ -2,7 +2,20 @@ class Api::V1::Webgui::SearchController < ApplicationController
     def index
         case params[:model]
             when 'lyric'
-                result = Lyric.search(query:params[:q],limit:params[:limit])
+                ins = Lyric.search(query:params[:q],limit:params[:limit])
+                result = ins.map do |data|
+                    next({
+                        title:data.title,
+                        artist:data.artist,
+                        key:data.key,
+                        jucket:data.jucket.to_s,
+                        lyricist:data.lyricist,
+                        composer:data.composer,
+                        lyrics:data.lyrics,
+                        amazonUrl:data.amazonUrl,
+                        iTunesUrl:data.iTunesUrl,
+                    })
+                end
             when 'article'
                 result = Lyric.search(query:params[:q],limit:params[:limit])
         end
--- app/models/lyric.rb
+++ app/models/lyric.rb
@@ -3,24 +3,12 @@ class Lyric < ApplicationRecord
     has_many :favs
     def self.search(props)
         return self.all.limit(props[:limit]) unless props[:query]
-        ins = self.where(
+        result = self.where(
             ['artist ? OR title LIKE ?', 
             "%#{props[:query].upcase}%", "%#{props[:query].upcase}%"]
         ).limit(props[:limit])
 
-        result = ins.map do |data|
-            next({
-                title:data.title,
-                artist:data.artist,
-                key:data.key,
-                jucket:data.jucket.to_s,
-                lyricist:data.lyricist,
-                composer:data.composer,
-                lyrics:data.lyrics,
-                amazonUrl:data.amazonUrl,
-                iTunesUrl:data.iTunesUrl,
-            })
-        end
+        
         return result
     end
     
