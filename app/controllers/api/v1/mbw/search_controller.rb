c2b16b62626159597b9f955085ecb8d85b66cedb
TakumiHiguchi <andf.for.gt@gmail.com>
2020-09-24 07:53:47 +0900

    add: searchAPIのlyricsにkaminariを導入

--- app/controllers/api/v1/mbw/search_controller.rb
+++ app/controllers/api/v1/mbw/search_controller.rb
@@ -2,14 +2,16 @@ class Api::V1::Mbw::SearchController < ApplicationController
     def index
         case params[:model]
             when 'lyric'
-                result = Lyric.joins(:favs).select("lyrics.*, favs.*").search_create_hash(query: params[:q], limit:params[:limit], page: params[:page])
+                result,pagenation = Lyric.joins(:favs).select("lyrics.*, favs.*").search_create_hash(query: params[:q], limit:params[:limit], page: params[:page])
+                
             when 'article'
                 result = Article.search_create_hash(query:params[:q],limit:params[:limit])
         end
         render json: JSON.pretty_generate({
             status:'SUCCESS',
             api_version: 'v1',
-            result:result
+            result:result,
+            pagenation:pagenation
         })
     end
 end
--- app/models/lyric.rb
+++ app/models/lyric.rb
@@ -5,7 +5,7 @@ class Lyric < ApplicationRecord
     def self.search(props)
       page = props[:page]
       page ||= 1
-      return self.all.page(page).per(props[:limit]) unless props[:query]
+      return self.page(page).per(props[:limit]) unless props[:query]
       result = self.where(['UPPER(artist) LIKE ? OR UPPER(title) LIKE ?', "%#{props[:query].upcase}%", "%#{props[:query].upcase}%"]).page(page).per(props[:limit])
                 
       return result
@@ -14,6 +14,14 @@ class Lyric < ApplicationRecord
   def self.search_create_hash(props)
     begin
       lyrics = self.search(props)
+      pagenation = {
+        current:  lyrics.current_page,
+        previous: lyrics.prev_page,
+        next:     lyrics.next_page,   
+        limit_value: lyrics.limit_value,
+        pages:    lyrics.total_pages,
+        count:    lyrics.total_count
+      }
       result = lyrics.map do |lyric|
         next({
           title:lyric.title,
@@ -25,11 +33,10 @@ class Lyric < ApplicationRecord
           lyrics:lyric.lyrics,
           amazonUrl:lyric.amazonUrl,
           iTunesUrl:lyric.iTunesUrl,
-
         })
       end
     
     end
-      return result
+      return result,pagenation
   end
 end
