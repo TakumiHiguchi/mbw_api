e7cfa22d8f2ec5881cd19d00ff8057a0dbd59a35
TakumiHiguchi <andf.for.gt@gmail.com>
2020-10-11 21:48:49 +0900

    ref: エラーのjsonの処理を直した

--- app/workers/renderJson.rb
+++ app/workers/renderJson.rb
@@ -1,71 +1,32 @@
 class RenderJson
   def createError(props)
+    props[:status] ||= 400
+    hash = {
+      :status => props[:status],
+      :api_version => props[:api_version],
+      :error_code => props[:code],
+    }
     case props[:code]
       when 'AE_0001'
-        json = JSON.pretty_generate({
-          status:'ERROR',
-          api_version: props[:api_version],
-          error_code: props[:code],
-          mes:'アクセス権限がありません'
-        })
+        hash[:mes] = 'アクセス権限がありません'
       when 'AE_0002'
-        json = JSON.pretty_generate({
-          status:'ERROR',
-          api_version: props[:api_version],
-          error_code: props[:code],
-          mes:'アクセス権限がありません。サインインしてください'
-        })
+        hash[:mes] = 'アクセス権限がありません。サインインしてください'
       when 'AE_0003'
-        json = JSON.pretty_generate({
-          status:'ERROR',
-          api_version: props[:api_version],
-          error_code: props[:code],
-          mes:'トークンの有効期限が切れています'
-        })
+        hash[:mes] = 'トークンの有効期限が切れています'
       when 'AE_0004'
-        json = JSON.pretty_generate({
-          status:'ERROR',
-          api_version: props[:api_version],
-          error_code: props[:code],
-          mes:'登録できませんでした。再度お試しください'
-        })
+        hash[:mes] = '登録できませんでした。再度お試しください'
       when 'AE_0005'
-        json = JSON.pretty_generate({
-          status:'ERROR',
-          api_version: props[:api_version],
-          error_code: props[:code],
-          mes:'パスワードは半角6~12文字英大文字・小文字・数字それぞれ１文字以上含む必要があります'
-        })
+        hash[:mes] = 'パスワードは半角6~12文字英大文字・小文字・数字それぞれ１文字以上含む必要があります'
       when 'AE_0006'
-        json = JSON.pretty_generate({
-          status:'ERROR',
-          api_version: props[:api_version],
-          error_code: props[:code],
-          mes:'メールアドレスかパスワードが正しくありません'
-        })
+        hash[:mes] = 'メールアドレスかパスワードが正しくありません'
       when 'AE_0007'
-        json = JSON.pretty_generate({
-          status:'ERROR',
-          api_version: props[:api_version],
-          error_code: props[:code],
-          mes:'すでに他の人に執筆されています'
-        })
+        hash[:mes] = 'すでに他の人に執筆されています'
       when 'AE_0014'
-        json = JSON.pretty_generate({
-          status:'ERROR',
-          api_version: props[:api_version],
-          error_code: props[:code],
-          mes:'ユーザーが存在しません。契約主に問い合わせてください'
-        })
+        hash[:mes] = 'ユーザーが存在しません。契約主に問い合わせてください'
       when 'AE_0101'
-        json = JSON.pretty_generate({
-          status:'ERROR',
-          api_version: props[:api_version],
-          error_code: props[:code],
-          mes:'タグが存在しません。'
-        })
+        hash[:mes] = 'タグが存在しません。'
     end
-    return json
+    return JSON.pretty_generate(hash)
   end
   
 end
