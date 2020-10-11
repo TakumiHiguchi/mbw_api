a1ecde9f52405f6e4698622f851a3d30de0c6fd4
TakumiHiguchi <andf.for.gt@gmail.com>
2020-10-11 18:04:37 +0900

    ref: 画像のurlを環境ごとに分けるようにした

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,15 +1,21 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
   def s3_presigner(props)
-    s3 = Aws::S3::Resource.new(
-      region: ENV['S3_REGION'],
-      credentials: Aws::Credentials.new(
-          ENV['S3_ACCESS_KEY'],
-          ENV['S3_SECRET_KEY']
+    if Rails.env.development?
+      return nil
+    elsif Rails.env.production?
+      s3 = Aws::S3::Resource.new(
+        region: ENV['S3_REGION'],
+        credentials: Aws::Credentials.new(
+            ENV['S3_ACCESS_KEY'],
+            ENV['S3_SECRET_KEY']
+        )
       )
-    )
-    signer = Aws::S3::Presigner.new(client: s3.client)
-    presigned_url = signer.presigned_url(:get_object,bucket: ENV['S3_BUCKET'], key: props[:path], expires_in: 60)
-    return presigned_url
+      signer = Aws::S3::Presigner.new(client: s3.client)
+      presigned_url = signer.presigned_url(:get_object,bucket: ENV['S3_BUCKET'], key: props[:path], expires_in: 60)
+      return presigned_url
+    else
+      return nil
+    end
   end
 end
