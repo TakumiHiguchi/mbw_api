d9246158148cc2c528a74b206c19322fd4eed338
TakumiHiguchi <andf.for.gt@gmail.com>
2020-10-21 15:24:55 +0900

    ref: renderJsonをbaseでnewして200ステータスの時もそこで処理するように変更

--- app/controllers/api/v1/mbw/tag_controller.rb
+++ app/controllers/api/v1/mbw/tag_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::Mbw::TagController < ApplicationController
+class Api::V1::Mbw::TagController < Api::V1::Mbw::BaseController
   def show
     tag = Tag.find_by(key: params[:id])
     if tag
@@ -6,14 +6,9 @@ class Api::V1::Mbw::TagController < ApplicationController
       tag_hash[:articles] = Article.joins(:tags).select('articles.*,tags.*,articles.key AS article_key,tags.key AS tag_key,articles.thumbnail AS articles_thumbnail').create_article_hash({ :search_type => 'tag', :query => tag.name, :limit => 20, :query => nil, :with_thumbnail => true, :with_tag => true })
       tag_hash[:lyrics] = Lyric.joins(:favs).select("lyrics.*, favs.*").search_create_hash(query: tag.name, limit: 20)
       tag_hash[:recomanded] = tag_hash[:articles]
-      render json: JSON.pretty_generate({
-        status:'SUCCESS',
-        api_version: 'v1',
-        result: tag_hash
-      })
+      render status: 200, json: @@renderJson.createSuccess({ :api_version => 'v1', :result => [{:result => tag_hash}] })
     else
-      errorJson = RenderJson.new
-      render json: errorJson.createError(code:'AE_0101',api_version:'v1')
+      render json: @@renderJson.createError(code:'AE_0101',api_version:'v1')
     end
   end
 end
