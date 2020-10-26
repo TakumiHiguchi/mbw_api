e66928b91b54d0804125118177593b7498cf7567
TakumiHiguchi <andf.for.gt@gmail.com>
2020-10-26 20:34:57 +0900

    ref: Api::V1::Webgui::UnapprovedArticleControllerのeditを修正した

--- app/controllers/api/v1/webgui/unapproved_article_controller.rb
+++ app/controllers/api/v1/webgui/unapproved_article_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::Webgui::UnapprovedArticleController < Api::V1::Webgui::BaseController
-  before_action :setWritter, :only => [:index, :create]
+  before_action :setWritter, :only => [:index, :create, :edit]
   def index
     result = @user.article_requests.map{ |data| data.create_default_hash }
     render status: 200, json: @@renderJson.createSuccess({ :api_version => 'v1', :result => [{:result => result}] })
@@ -10,40 +10,23 @@ class Api::V1::Webgui::UnapprovedArticleController < Api::V1::Webgui::BaseContro
     if @article_request.present?
       @user.assign_article_request(@article_request)
       @article_request.unapproved_articles.build(unapproved_articles_create_params)
-      article_request.save
+      @article_request.save
       render status: 200, json: @@renderJson.createSuccess({ :api_version => 'v1', :result => [] })
     else
-      render json: @@renderJson.createError(code:'AE_0007',api_version:'v1')
+      render status: 400, json: @@renderJson.createError(code:'AE_0007',api_version:'v1')
     end
   end
-    def edit
-        auth = Authentication.new()
-        errorJson = RenderJson.new()
-        result = auth.isWriter?(email:params[:email],session:params[:session])
-        if result[:isWriter]
-            ins = Writer.joins(article_requests: :unapproved_articles).select("writers.id, article_requests.id,article_requests.maxage, article_requests.count,unapproved_articles.*").where("writers.id = ?", result[:writer].id).find_by("article_requests.key = ?", params[:id])
-            if ins
-                result ={
-                    title:ins.title,
-                    content:ins.content,
-                    count:ins.count,
-                    description:ins.description,
-                    key:ins.key,
-                    maxAge:ins.maxage
-                }
-                render json: JSON.pretty_generate({
-                    status:'SUCCESS',
-                    api_version: 'v1',
-                    result:result
-                })
-            else
-                render json: errorJson.createError(code:'AE_0001',api_version:'v1')
-            end
-            
-        else
-            render json: errorJson.createError(code:'AE_0002',api_version:'v1')
-        end
+
+  def edit
+    unapproved_article = @user.article_requests.find_by(:key => params[:id]).unapproved_articles.first
+    if unapproved_article.present?
+      result = unapproved_article.create_default_hash
+      render status: 200, json: @@renderJson.createSuccess({ :api_version => 'v1', :result => [{:result => result}] })
+    else
+      render status: 400, json: @@renderJson.createError(code:'AE_0001',api_version:'v1')
     end
+  end
+
     def update
         auth = Authentication.new()
         errorJson = RenderJson.new()
