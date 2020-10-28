f69396b6608c95499010e6135c5ab137e6e2e34e
TakumiHiguchi <andf.for.gt@gmail.com>
2020-10-28 15:04:51 +0900

    ref: Api::V1::Webgui::ArticleControllerのcreateの修正

--- app/controllers/api/v1/webgui/base_controller.rb
+++ app/controllers/api/v1/webgui/base_controller.rb
@@ -13,4 +13,8 @@ class Api::V1::Webgui::BaseController < ApplicationController
     @user = Writer.find_by(email:params[:email],session:params[:session], admin: true)
     render status: 401, json: @@renderJson.createError(code:'AE_0001',api_version:'v1') if @user.nil?
   end
+
+  def set_articles
+    @article = Article
+  end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -117,41 +117,41 @@ class Article < ApplicationRecord
     ).page(page).per(props[:limit])
   end
 
-    def image_from_base64(b64)
-        uri = URI.parse(b64)
-        if uri.scheme == "data" then
-            data = decode(uri)
-            extension = extension(uri)
-            file = decode64_tempfile(data,extension)
-            self.update(thumbnail:file)
-        end
+  def image_from_base64(b64)
+    uri = URI.parse(b64)
+    if uri.scheme == "data" then
+        data = decode(uri)
+        extension = extension(uri)
+        file = decode64_tempfile(data,extension)
+        self.update(thumbnail:file)
     end
-    def decode(uri)
-        opaque = uri.opaque
-        data = opaque[opaque.index(",") + 1, opaque.size]
-        Base64.decode64(data)
-    end
-    
-    def extension(uri)
-        opaque = uri.opaque
-        mime_type = opaque[0, opaque.index(";")]
-        p opaque
-        case mime_type
-        when "image/png" then
-          ".png"
-        when "image/jpeg" then
-          ".jpg"
-        when "image/jpg" then
-          ".jpg"
-        else
-          raise "Unsupport Content-Type"
-        end
-    end
-    def decode64_tempfile(f,extension)
-        file = Tempfile.new(['test', extension])
-        file.binmode
-        file << f
-        file.rewind
-        return file
-    end
+  end
+  def decode(uri)
+      opaque = uri.opaque
+      data = opaque[opaque.index(",") + 1, opaque.size]
+      Base64.decode64(data)
+  end
+
+  def extension(uri)
+      opaque = uri.opaque
+      mime_type = opaque[0, opaque.index(";")]
+      p opaque
+      case mime_type
+      when "image/png" then
+        ".png"
+      when "image/jpeg" then
+        ".jpg"
+      when "image/jpg" then
+        ".jpg"
+      else
+        raise "Unsupport Content-Type"
+      end
+  end
+  def decode64_tempfile(f,extension)
+      file = Tempfile.new(['test', extension])
+      file.binmode
+      file << f
+      file.rewind
+      return file
+  end
 end
