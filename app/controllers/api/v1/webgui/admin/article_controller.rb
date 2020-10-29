e33d550242700e371b6a625797bd78350a8cf79f
TakumiHiguchi <andf.for.gt@gmail.com>
2020-10-29 09:40:17 +0900

    ref: Api::V1::Webgui::Admin::ArticleController#createを修正

--- app/controllers/api/v1/webgui/admin/article_controller.rb
+++ app/controllers/api/v1/webgui/admin/article_controller.rb
@@ -1,22 +1,13 @@
 class Api::V1::Webgui::Admin::ArticleController < Api::V1::Webgui::Admin::BaseController
+  before_action :setAdminUser, :only => [:create, :edit]
   def create
     #普通に記事を作成するメソッド
-    auth = Authentication.new()
-    errorJson = RenderJson.new()
-    if auth.isAdmin?(email:params[:email],session:params[:session]) then
-      article = Article.create(create_article_params)
-      article.image_from_base64(params[:thumbnail])
-      params[:tags].each do |tag|
-        article.tags.createTag(article.id,tag_name)
-      end
-      render json: JSON.pretty_generate({
-        status:'SUCCESS',
-        api_version: 'v1'
-      })
-    else
-      render json: errorJson.createError(code:'AE_0001',api_version:'v1')
-    end
+    article = Article.create(create_article_params)
+    article.image_from_base64(params[:thumbnail])
+    params[:tags].each{|tag_name| article.tags.createTag(article.id,tag_name) }
+    render status: 200, json: @@renderJson.createSuccess({ :api_version => 'v1', :result => [] })
   end
+
   def edit
     data = Article.find_by(key: params[:id])
     tag_list = data.tags.map do |tag|
@@ -46,7 +37,7 @@ class Api::V1::Webgui::Admin::ArticleController < Api::V1::Webgui::Admin::BaseCo
       content:params[:content],
       description:params[:description],
       release_time:params[:releaseTime],
-      key: @base_worker.get_key
+      key: @@base_worker.get_key
     })
   end
 end
--- app/controllers/api/v1/webgui/admin/base_controller.rb
+++ app/controllers/api/v1/webgui/admin/base_controller.rb
@@ -1,3 +1,10 @@
 class Api::V1::Webgui::Admin::BaseController < ApplicationController
-  @base_worker = BaseWorker.new()
+  @@base_worker = BaseWorker.new()
+  @@renderJson = RenderJson.new()
+
+  def setAdminUser
+    @auth = Authentication.new()
+    @user = Writer.find_by(email:params[:email],session:params[:session], admin: true)
+    render status: 401, json: @@renderJson.createError(code:'AE_0001',api_version:'v1') if @user.nil?
+  end
 end
