9b8263deb396c91fdb5ee5fd1ca9d51ea4bff40b
TakumiHiguchi <andf.for.gt@gmail.com>
2020-11-04 12:22:25 +0900

    ref:  key :query is duplicated and overwrittenの修正

--- app/controllers/api/v1/mbw/tag_controller.rb
+++ app/controllers/api/v1/mbw/tag_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::Mbw::TagController < Api::V1::Mbw::BaseController
     tag = Tag.find_by(key: params[:id])
     if tag
       tag_hash = tag.create_hash
-      tag_hash[:articles] = Article.joins(:tags).select('articles.*,tags.*,articles.key AS article_key,tags.key AS tag_key,articles.thumbnail AS articles_thumbnail').create_article_hash({ :search_type => 'tag', :query => tag.name, :limit => 20 :with_thumbnail => true, :with_tag => true })
+      tag_hash[:articles] = Article.joins(:tags).select('articles.*,tags.*,articles.key AS article_key,tags.key AS tag_key,articles.thumbnail AS articles_thumbnail').create_article_hash({ :search_type => 'tag', :query => tag.name, :limit => 20, :with_thumbnail => true, :with_tag => true })
       tag_hash[:lyrics] = Lyric.joins(:favs).select("lyrics.*, favs.*").search_create_hash(query: tag.name, limit: 20)
       tag_hash[:recomanded] = tag_hash[:articles]
       render status: 200, json: @@renderJson.createSuccess({ :api_version => 'v1', :result => [{:result => tag_hash}] })
